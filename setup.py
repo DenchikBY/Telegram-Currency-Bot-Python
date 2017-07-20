40f760fd4ed12c5604e42b74bf3eeaa5497cb9a3
Denis Butko <dbutko@itsupportme.com>
2017-07-20 19:40:18 +0300

    Added setup.py, removed __init__.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='telegram-currency-bot',
+    version='0.1',
+    packages=['telegram-currency-bot'],
+    url='https://github.com/DenchikBY/Telegram-Currency-Bot-Python',
+    license='MIT',
+    author='Denis Butko',
+    author_email='butko.denis.95@gmail.com',
+    description='Telegram bot for converting BYN to USD, EUR and RUR',
+    install_requires=[
+        'requests',
+        'python-telegram-bot'
+    ],
+)
