f7c434ea4f4d0fba5a973b4211670522efa3859e
Denis Butko <dbutko@itsupportme.com>
2017-07-26 18:53:24 +0300

    Added /help command and check for different currencies

--- telegrambot.py
+++ telegrambot.py
@@ -1,8 +1,10 @@
 import atexit
 import sys
 
+import re
+import telegram
 from telegram import Update, Bot
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 
 from UpdateThread import UpdateThread
 from currencies import currencies
@@ -14,18 +16,53 @@ else:
     print('Token not provided!')
     sys.exit()
 
+regex = {
+    'BYN': r'^(\d+((\.|\,)\d+)?)(\s*BYN)?$',
+    'USD': r'^(\d+((\.|\,)\d+)?)\s*(\$|USD)$',
+    'EUR': r'^(\d+((\.|\,)\d+)?)\s*(\€|EUR)$',
+    'RUR': r'^(\d+((\.|\,)\d+)?)\s*(\₽|RUR)$',
+}
+
+
+def find_currency(text: str):
+    for key, r in regex.items():
+        result = re.match(r, text)
+        if result is not None:
+            return {
+                'currency': key,
+                'amount': float(result.groups()[0].replace(',', '.'))
+            }
+    return None
+
 
 def message(bot: Bot, update: Update) -> None:
     try:
-        input_val = float(update.message.text.replace(',', '.'))
-        response = '\U0001F1E7\U0001F1FE BYN ' + '{0:g}'.format(input_val) + 'руб.'
-        for currency in currencies:
-            amount = input_val / currency.amount
-            response += '\n' + currency.icon + ' ' + currency.name + ' ' + '{0:g}'.format(amount) + currency.suf
+        input_data = find_currency(update.message.text)
+        if input_data is None:
+            response = 'Неверный формат!'
+        elif input_data['currency'] == 'BYN':
+            response = '\U0001F1E7\U0001F1FE BYN ' + '{0:g}'.format(input_data['amount']) + 'руб.'
+            for currency in currencies:
+                amount = input_data['amount'] / currency.amount
+                response += '\n' + currency.icon + ' ' + currency.name + ' ' + '{0:g}'.format(amount) + currency.suf
+        else:
+            response = 'Выбранная валюта пока не поддерживается!'
     except:
-        response = 'Неверный формат!'
+        response = 'Произошла непредвиденная ошибка!'
     update.message.reply_text(response)
     store_log(update, response)
+
+
+def command_help(bot: Bot, update: Update) -> None:
+    update.message.reply_text('*Отправьте сообщение в одном из форматов:*\n'
+                              '1000 - будет считаться как BYN\n'
+                              '1000 BYN\n'
+                              '1000 USD\n'
+                              '1000 $\n'
+                              '1000 EUR\n'
+                              '1000 €\n'
+                              '1000 RUR\n'
+                              '1000 ₽\n', parse_mode=telegram.ParseMode.MARKDOWN)
 
 
 update_thread = UpdateThread()
@@ -34,6 +71,7 @@ update_thread.start()
 updater = Updater(token)
 
 updater.dispatcher.add_handler(MessageHandler(Filters.text, message))
+updater.dispatcher.add_handler(CommandHandler('help', command_help))
 
 updater.start_polling()
 updater.idle()
